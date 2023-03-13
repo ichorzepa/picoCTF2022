5acf8cb208e279118c10ea941bf11797aedb27bc
ichorzepa <33585272+ichorzepa@users.noreply.github.com>
2023-03-13 16:22:33 -0400

    java decompiling

---
+++ fresh_java/Key.java
@@ -0,0 +1,123 @@
+
+
+import java.util.Scanner;
+
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+public class Key
+{
+    public static void main(final String[] array) {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter key:");
+        final String nextLine = scanner.nextLine();
+        if (nextLine.length() != 34) {
+            System.out.println("Good");
+        }
+        if (nextLine.charAt(33) != '}') {
+            System.out.println("}");
+        }
+        if (nextLine.charAt(32) != '9') {
+            System.out.println("9");
+        }
+        if (nextLine.charAt(31) != '8') {
+            System.out.println("8");
+        }
+        if (nextLine.charAt(30) != 'c') {
+            System.out.println("c");
+        }
+        if (nextLine.charAt(29) != 'a') {
+            System.out.println("a");
+        }
+        if (nextLine.charAt(28) != 'c') {
+            System.out.println("c");
+        }
+        if (nextLine.charAt(27) != '8') {
+            System.out.println("8");
+        }
+        if (nextLine.charAt(26) != '3') {
+            System.out.println("3");
+        }
+        if (nextLine.charAt(25) != '7') {
+            System.out.println("7");
+        }
+        if (nextLine.charAt(24) != '_') {
+            System.out.println("_");
+        }
+        if (nextLine.charAt(23) != 'd') {
+            System.out.println("d");
+        }
+        if (nextLine.charAt(22) != '3') {
+            System.out.println("3");
+        }
+        if (nextLine.charAt(21) != 'r') {
+            System.out.println("r");
+        }
+        if (nextLine.charAt(20) != '1') {
+            System.out.println("1");
+        }
+        if (nextLine.charAt(19) != 'u') {
+            System.out.println("u");
+        }
+        if (nextLine.charAt(18) != 'q') {
+            System.out.println("q");
+        }
+        if (nextLine.charAt(17) != '3') {
+            System.out.println("3");
+        }
+        if (nextLine.charAt(16) != 'r') {
+            System.out.println("r");
+        }
+        if (nextLine.charAt(15) != '_') {
+            System.out.println("_");
+        }
+        if (nextLine.charAt(14) != 'g') {
+            System.out.println("g");
+        }
+        if (nextLine.charAt(13) != 'n') {
+            System.out.println("n");
+        }
+        if (nextLine.charAt(12) != '1') {
+            System.out.println("1");
+        }
+        if (nextLine.charAt(11) != 'l') {
+            System.out.println("l");
+        }
+        if (nextLine.charAt(10) != '0') {
+            System.out.println("0");
+        }
+        if (nextLine.charAt(9) != '0') {
+            System.out.println("0");
+        }
+        if (nextLine.charAt(8) != '7') {
+            System.out.println("7");
+        }
+        if (nextLine.charAt(7) != '{') {
+            System.out.println("{");
+        }
+        if (nextLine.charAt(6) != 'F') {
+            System.out.println("F");
+        }
+        if (nextLine.charAt(5) != 'T') {
+            System.out.println("T");
+        }
+        if (nextLine.charAt(4) != 'C') {
+            System.out.println("C");
+        }
+        if (nextLine.charAt(3) != 'o') {
+            System.out.println("o");
+        }
+        if (nextLine.charAt(2) != 'c') {
+            System.out.println("c");
+        }
+        if (nextLine.charAt(1) != 'i') {
+            System.out.println("i");
+        }
+        if (nextLine.charAt(0) != 'p') {
+            System.out.println("p");
+        }
+        System.out.println("Valid key");
+    }
+}
+
---
+++ fresh_java/README.md
@@ -0,0 +1,21 @@
+
+# Fresh Java
+
+Challenge from picoCTF 2022.
+
+
+
+## Challenge description
+
+Can you get the flag? Reverse engineer this [Java program](https://artifacts.picoctf.net/c/144/KeygenMe.class).
+
+## Explained
+
+Download file using ```wget```. Hint advice us to use a decompiler for Java. Use [Java decompiler online](http://www.javadecompilers.com/). Save code inside new file in your working directory.
+
+Now we can analyse that key should have 34 characters and then read flag. Let's edit code to print flag it self.
+
+
+## 🔗 Links
+[[https://play.picoctf.org/practice/challenge/271]](https://play.picoctf.org/practice/challenge/271)
+
